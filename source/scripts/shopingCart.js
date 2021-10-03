9370757a8b7cd6515256401e9a8c8ebc6beefb80
william <diazrubianowilliam94@gmail.com>
2021-10-03 18:40:38 -0500

    se eliminan algunos archivos no necesarios.

--- source/scripts/shopingCart.js
+++
@@ -1,9 +0,0 @@
-
-// const btn=document.querySelector(".main-menu-btn")
-// btn.addEventListener("click", () => {
-// document.querySelector(".main-menu-ul").classList.toggle("show");
-// });
-
-import {btn,boton} from "./main.js"
-
-console.log(setcarrito)
